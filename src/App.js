24b34db1350092f11dd416544ab28fa638f453ba
yanagikubo <yanagikuboyuki@team-lab.com>
2020-05-02 15:22:49 +0900

    丸をレンダリング

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from 'react';
+import { Canvas, useFrame } from 'react-three-fiber';
 
-function App() {
+const Atom = () => {
+  const ref = useRef();
+  useFrame(() => (ref.current.rotation.z += 0.01));
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <mesh
+          ref={ref}
+          position={ [0, 0, 0] }
+          onClick={e => console.log('click')}
+          onPointerOver={e => console.log('hover')}
+          onPointerOut={e => console.log('unhover')}
+      >
+          <sphereBufferGeometry attach="geometry" args={[1, 32, 32]} />
+          <meshBasicMaterial
+              attach='material'
+              color='#9acc14'
+              opacity={0.5}
+              transparent
+          />
+      </mesh>
   );
-}
+};
+
+const App = () => {
+  return (
+      <Canvas>
+          <Atom />
+      </Canvas>
+  );
+};
 
 export default App;
